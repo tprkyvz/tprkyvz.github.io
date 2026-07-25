d6b9b0cf91e32cb53201df74d4dd9cee5b4864f1
tprkyvz <toprakyvz@gmail.com>
2026-07-25 11:39:16 +0300

    chore: add GitHub Pages deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/blog-site',
+    assetPrefix: '/blog-site',
     images: {
         unoptimized: true,
     },
