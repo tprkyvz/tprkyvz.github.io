3886d18bb74dddd9aa873e5c032c5033adde4abb
tprkyvz <toprakyvz@gmail.com>
2026-07-25 11:56:15 +0300

    chore: remove basePath for root domain deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/blog-site',
-    assetPrefix: '/blog-site',
     images: {
         unoptimized: true,
     },
